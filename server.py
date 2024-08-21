73945dccd4990a13a6ec000f124343a8501d7926
kehman18 <kehindeadekola96@gmail.com>
2024-08-21 13:31:09 +0100

    changed the link to a relative path

--- server.py
+++ server.py
@@ -1,14 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-from PIL import Image
+'''data saving, hiding keys, flask attributes and picture processing'''
 import csv
 import os
 import uuid
-
+from flask import Flask, render_template, request, redirect, url_for
+from PIL import Image
 
 app = Flask(__name__)
 
 # Define the path to save uploaded images
-UPLOAD_FOLDER = r'C:\\Users\\Owner\\Desktop\\ediconference\\static\\uploaded_images'
+UPLOAD_FOLDER = r'.\static\\uploaded_images'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
@@ -86,7 +86,7 @@ def submit_form():
                 uploaded_image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_upload.filename)
                 image_upload.save(uploaded_image_path)
                 
-                image_1_path = r'C:\\Users\\Owner\\Desktop\\ediconference\\static\\EC_templates.png'
+                image_1_path = r'C:.\static\\EC_templates.png'
                 merged_image_filename = f'merged_{uuid.uuid4()}.png'
                 output_path = os.path.join(app.config['UPLOAD_FOLDER'], merged_image_filename)
 
