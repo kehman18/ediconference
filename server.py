b4387954985c3edf0c219a2392d3b544005a492f
kehman18 <kehindeadekola96@gmail.com>
2024-08-21 10:42:33 +0100

    added a framing picture and gitignore

--- server.py
+++ server.py
@@ -1,7 +1,15 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
+from PIL import Image
 import csv
+import os
+import uuid
+
 
 app = Flask(__name__)
+
+# Define the path to save uploaded images
+UPLOAD_FOLDER = r'C:\\Users\\Owner\\Desktop\\ediconference\\static\\uploaded_images'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route("/")
 def my_home():
@@ -11,6 +19,30 @@ def my_home():
 def html_page(page_name):
     return render_template(page_name)
 
+# Function to center and merge two images
+def center_image(image_1_path, image_2_path, output_path):
+    with Image.open(image_1_path) as img1, Image.open(image_2_path) as img2:
+        img2 = img2.resize((250, 250))
+
+        if img2.mode != 'RGBA':
+            img2 = img2.convert('RGBA')
+        width1, height1 = img1.size
+        center_x = width1 // 2
+        center_y = height1 // 2
+
+        paste_x = center_x - 125
+        paste_y = center_y - 125
+
+        composite = Image.new('RGBA', img1.size)
+        composite.paste(img1, (0, 0))
+        composite.paste(img2, (paste_x, paste_y), img2)
+
+        if img1.mode != 'RGBA':
+            composite = composite.convert('RGB')
+
+        composite.save(output_path)
+
+# Function to write user data to file
 def write_to_file(data):
     with open('database.txt', mode='a') as database:
         first_name = data["first name"]
@@ -21,8 +53,9 @@ def write_to_file(data):
         location = data['Location']
         Volunteers = data['volunteers']
         expectations = data['message']
-        database.write(f'\n{first_name}, {last_name}, {sex}, {email}, {phone_number}, {location}, {Volunteers}  {expectations}')
+        database.write(f'\n{first_name}, {last_name}, {sex}, {email}, {phone_number}, {location}, {Volunteers}, {expectations}')
 
+# Function to write user data to CSV
 def write_to_csv(data):
     with open('database.csv', mode='a', newline='') as database2:
         first_name = data["first name"]
@@ -36,14 +69,37 @@ def write_to_csv(data):
         csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
         csv_writer.writerow([first_name, last_name, sex, email, phone_number, location, Volunteers, expectations])
 
-@app.route('/submit_form', methods=['POST', 'GET'])
+# Route to handle form submission
+@app.route('/submit_form', methods=['POST'])
 def submit_form():
     if request.method == 'POST':
         data = request.form.to_dict()
         write_to_csv(data)
-        return redirect('/thankyou_index.html')
+
+        merged_image_filename = None
+        if 'image_upload' in request.files:
+            image_upload = request.files['image_upload']
+            if image_upload.filename != '':
+                if not os.path.exists(app.config['UPLOAD_FOLDER']):
+                    os.makedirs(app.config['UPLOAD_FOLDER'])
+                
+                uploaded_image_path = os.path.join(app.config['UPLOAD_FOLDER'], image_upload.filename)
+                image_upload.save(uploaded_image_path)
+                
+                image_1_path = r'C:\\Users\\Owner\\Desktop\\ediconference\\static\\EC_templates.png'
+                merged_image_filename = f'merged_{uuid.uuid4()}.png'
+                output_path = os.path.join(app.config['UPLOAD_FOLDER'], merged_image_filename)
+
+                center_image(image_1_path, uploaded_image_path, output_path)
+
+        return redirect(url_for('thankyou_index', merged_image=merged_image_filename))
     else:
-        return "Something went wrong. Please try again."
+        return "Oops, something went wrong. Please try again."
+
+@app.route('/thankyou_index.html')
+def thankyou_index():
+    merged_image = request.args.get('merged_image', None)
+    return render_template('thankyou_index.html', merged_image=merged_image)
 
 if __name__ == "__main__":
     app.run(debug=True)
